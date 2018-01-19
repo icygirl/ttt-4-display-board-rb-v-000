2bb8f256df2e402bbaab291bc722c1dd15d3a02c
Virginia Herron <inadream@gmail.com>
2018-01-19 06:39:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  cell = "   "
+  pipe = "|"
+  seperator = "-----------"
+  print cell, pipe, cell, pipe, cell
+  puts
+  puts seperator
+  print cell, pipe, cell, pipe, cell
+  puts
+  puts seperator
+  print cell, pipe, cell, pipe, cell
+  puts
+end
+
+display_board
